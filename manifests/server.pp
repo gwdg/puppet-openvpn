9d0ba234cd5492cc9ff25b2194b099b9259bc799
Daniel Adler <dadler1@gwdg.de>
2014-03-21 11:31:54 +0100

    added server option 'duplicate_cn'.

--- manifests/server.pp
+++ manifests/server.pp
@@ -20,20 +20,15 @@
 # [*email*]
 #   String.  Email address to be used for the SSL certificate
 #
-# [*common_name*]
-#   String.  Common name to be used for the SSL certificate
-#   Default: server
-#
 # [*compression*]
 #   String.  Which compression algorithim to use
 #   Default: comp-lzo
 #   Options: comp-lzo or '' (disable compression)
 #
 # [*dev*]
-#   String.  TUN/TAP virtual network device
-#   Default: tun0
-#   Options: tunX (routed connections), tapX (bridged connections). X
-#   can be omitted for a dynamic device.
+#   String.  Device method
+#   Default: tun
+#   Options: tun (routed connections), tap (bridged connections)
 #
 # [*user*]
 #   String.  Group to drop privileges to after startup
@@ -46,10 +41,6 @@
 # [*ipp*]
 #   Boolean.  Persist ifconfig information to a file to retain client IP
 #     addresses between sessions
-#   Default: false
-#
-# [*duplicate_cn*]
-#   Boolean.  Allow multiple connections on one cn
 #   Default: false
 #
 # [*local*]
@@ -78,14 +69,6 @@
 # [*server*]
 #   String.  Network to assign client addresses out of
 #   Default: None.  Required in tun mode, not in tap mode
-#
-# [*server_ipv6*]
-#   String.  IPv6 network to assign client addresses out of
-#   Default: None.
-#
-# [*server_bridge*]
-#   String. Server configuration to comply with existing DHCP server
-#   Default: None.
 #
 # [*push*]
 #   Array.  Options to push out to the client.  This can include routes, DNS
@@ -126,129 +109,9 @@
 #   Boolean, Enable/Disable.
 #   Default: false
 #
-# [*management*]
-#   Boolean.  Enable management interface
+# [*cn_duplicate*]
+#   Boolean, Enable/Disable.
 #   Default: false
-#
-# [*management_ip*]
-#   String.  IP address where the management interface will listen
-#   Default: localhost
-#
-# [*management_port*]
-#   String.  Port where the management interface will listen
-#   Default: 7505
-#
-# [*up*]
-#   String,  Script which we want to run when openvpn server starts
-#
-# [*down*]
-#   String,  Script which we want to run when openvpn server stops
-#
-# [*username_as_common_name*]
-#   Boolean. If true then set username-as-common-name
-#   Default: false
-#
-# [*client_cert_not_required*]
-#   Boolean. If true then set client-cert-not-required
-#   Default: false
-#
-# [*ldap_enabled*]
-#   Boolean. If ldap is enabled, do stuff
-#   Default: false
-#
-# [*ldap_server*]
-#   String. URL of LDAP server. ie. ldap://URL:PORT
-#   Default: None
-#
-# [*ldap_binddn*]
-#   String. LDAP DN to bind as
-#   Default: None
-#
-# [*ldap_bindpass*]
-#   String. LDAP password for ldapbinddn
-#   Default: None
-#
-# [*ldap_u_basedn*]
-#   String. Place in the LDAP tree to look for users
-#   Default: None
-#
-# [*ldap_u_filter*]
-#   String. User SearchFilter for LDAP accounts
-#   Default: None
-#
-# [*ldap_g_basedn*]
-#   String. Place in the LDAP tree to look for groups
-#   Default: None
-#
-# [*ldap_gmember*]
-#   Boolean. If defined use group block in ldap.conf
-#   Default: false
-#
-# [*ldap_g_filter*]
-#   String. Group SearchFilter for LDAP accounts
-#   Default: None
-#
-# [*ldap_memberatr*]
-#   String. Attribute for MemberAttribute. Used with ldapfilter
-#   Default: None
-#
-# [*ldap_tls_enable*]
-#   Boolean. Enable TLS for the LDAP authentication
-#   Default: false
-#
-# [*ldap_tls_ca_cert_file*]
-#   String. LDAP TLS authentication: path to the CA certificate.
-#   Default: None
-#
-# [*ldap_tls_ca_cert_dir*]
-#   String. LDAP TLS authentication: path to the CA certificates.
-#   Default: None
-#
-# [*ldap_tls_client_cert_file*]
-#   String. LDAP TLS authentication: path to the tls client certificate
-#   Default: None
-#
-# [*ldap_tls_client_key_file*]
-#   String. LDAP TLS authentication: path to the tls client key
-#   Default: None
-#
-# [*verb*]
-#   Integer.  Level of logging verbosity
-#   Default: 3
-#
-# [*cipher*]
-#   String,  Cipher to use for packet encryption
-#   Default: None
-#
-# [*persist_key*]
-#   Boolean.  Try to retain access to resources that may be unavailable
-#     because of privilege downgrades
-#   Default: false
-#
-# [*persist_tun*]
-#   Boolean.  Try to retain access to resources that may be unavailable
-#     because of privilege downgrades
-#   Default: false
-#
-# [*key_expire*]
-#   String.  The number of days to certify the server certificate for
-#   Default: 3650
-#
-# [*ca_expire*]
-#   String.  The number of days to certify the CA certificate for
-#   Default: 3650
-#
-# [*key_name*]
-#   String.  Value for name_default variable in openssl.cnf (and KEY_NAME in vars)
-#   Default: None
-#
-# [*key_ou*]
-#   String.  Value for organizationalUnitName_default variable in openssl.cnf (and KEY_OU in vars)
-#   Default: None
-#
-# [*key_cn*]
-#   String.  Value for commonName_default variable in openssl.cnf (and KEY_CN in vars)
-#   Default: None
 #
 # === Examples
 #
@@ -290,21 +153,18 @@ define openvpn::server(
   $city,
   $organization,
   $email,
-  $common_name = 'server',
   $compression = 'comp-lzo',
   $dev = 'tun0',
   $user = 'nobody',
   $group = false,
   $ipp = false,
-  $duplicate_cn = false,
+  $ip_pool = [],
   $local = $::ipaddress_eth0,
   $logfile = false,
   $port = '1194',
   $proto = 'tcp',
   $status_log = "${name}/openvpn-status.log",
   $server = '',
-  $server_ipv6 = '',
-  $server_bridge = '',
   $push = [],
   $route = [],
   $keepalive = '',
@@ -314,37 +174,7 @@ define openvpn::server(
   $tcp_nodelay = false,
   $ccd_exclusive = false,
   $pam = false,
-  $management = false,
-  $management_ip = 'localhost',
-  $management_port = 7505,
-  $up = '',
-  $down = '',
-  $username_as_common_name = false,
-  $client_cert_not_required = false,
-  $ldap_enabled = false,
-  $ldap_server = '',
-  $ldap_binddn = '',
-  $ldap_bindpass = '',
-  $ldap_u_basedn = '',
-  $ldap_g_basedn = '',
-  $ldap_gmember = false,
-  $ldap_u_filter = '',
-  $ldap_g_filter = '',
-  $ldap_memberatr = '',
-  $ldap_tls_enable = false,
-  $ldap_tls_ca_cert_file = '',
-  $ldap_tls_ca_cert_dir  = '',
-  $ldap_tls_client_cert_file = '',
-  $ldap_tls_client_key_file  = '',
-  $ca_expire = 3650,
-  $key_expire = 3650,
-  $key_cn = '',
-  $key_name = '',
-  $key_ou = '',
-  $verb = '',
-  $cipher = '',
-  $persist_key = false,
-  $persist_tun = false,
+  $duplicate_cn = false,
 ) {
 
   include openvpn
@@ -364,7 +194,6 @@ define openvpn::server(
 
   file {
     [ "/etc/openvpn/${name}",
-      "/etc/openvpn/${name}/auth",
       "/etc/openvpn/${name}/client-configs",
       "/etc/openvpn/${name}/download-configs" ]:
         ensure  => directory;
@@ -426,9 +255,9 @@ define openvpn::server(
                     File["/etc/openvpn/${name}/easy-rsa/openssl.cnf"] ];
 
     "generate server cert ${name}":
-      command  => ". ./vars && ./pkitool --server ${common_name}",
+      command  => '. ./vars && ./pkitool --server server',
       cwd      => "/etc/openvpn/${name}/easy-rsa",
-      creates  => "/etc/openvpn/${name}/easy-rsa/keys/${common_name}.key",
+      creates  => "/etc/openvpn/${name}/easy-rsa/keys/server.key",
       provider => 'shell',
       require  => Exec["initca ${name}"];
   }
@@ -442,7 +271,7 @@ define openvpn::server(
 
   exec {
     "create crl.pem on ${name}":
-      command  => ". ./vars && KEY_CN='' KEY_OU='' KEY_NAME='' KEY_ALTNAMES='' openssl ca -gencrl -out /etc/openvpn/${name}/crl.pem -config /etc/openvpn/${name}/easy-rsa/openssl.cnf",
+      command  => ". ./vars && KEY_CN='' KEY_OU='' KEY_NAME='' openssl ca -gencrl -out /etc/openvpn/${name}/crl.pem -config /etc/openvpn/${name}/easy-rsa/openssl.cnf",
       cwd      => "/etc/openvpn/${name}/easy-rsa",
       creates  => "/etc/openvpn/${name}/crl.pem",
       provider => 'shell',
@@ -471,13 +300,5 @@ define openvpn::server(
       group   => root,
       mode    => '0444',
       content => template('openvpn/server.erb');
-  }
-  if $ldap_enabled == true {
-    file {
-      "/etc/openvpn/${name}/auth/ldap.conf":
-        ensure  => present,
-        content => template('openvpn/ldap.erb'),
-        require => Package["openvpn-auth-ldap"],
-    }
   }
 }
